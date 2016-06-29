a3485ac1f26b5f1e06979da689e192d1d33cbce5
abhi7300 <abhishek1010mukherjee@gmail.com>
2016-06-30 01:54:09 +0530

    youtube player added

--- app/src/main/java/com/example/videoplayback/MainActivity.java
+++ app/src/main/java/com/example/videoplayback/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.videoplayback;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    Button b;
+    private YouTubePlayerView youtubePlayerView;
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //careful, API key: AIzaSyAOPcJG6cDtjADjmAA6ggucpyKBJf-Au0E
+
+        youtubePlayerView = (YouTubePlayerView) findViewById(R.id.youTubePlayerView);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("EXeTwQWrcwY"); //key from the video URL
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        b = (Button) findViewById(R.id.playButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youtubePlayerView.initialize("AIzaSyAOPcJG6cDtjADjmAA6ggucpyKBJf-Au0E", onInitializedListener);
+            }
+        });
+
     }
 }
